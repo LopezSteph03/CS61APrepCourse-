03a5240d222ccfdabf813d8351d02e908e4239ae
Stephanie Lopez <Stay-lo@Stephanies-Air.att.net>
2016-12-23 18:49:50 -0800

    save chpt 2

---
+++ chpt2/calc.rb
@@ -0,0 +1,47 @@
+puts 1+2
+puts 'I like ' + 'apple pie.'
+puts 'blink ' * 4 
+puts 12 + 12 
+puts '12' + '12' 
+puts '12 + 12' 
+puts 'You\'re swell!' 
+puts 'up\\down'
+puts 'up\down'
+my_string = '...you can say that again...' 
+puts my_string 
+puts my_string 
+name = ' Anya Christina Emmanuella jenkins Harris'
+puts 'My name is ' + name + '.'
+puts 'Wow! ' + name 
+puts 'is a really long name!' 
+var1 = 2
+var2 = '5'
+puts var1.to_s + var2 
+
+puts '15'.to_f 
+puts '99.999'.to_f 
+puts '99.999'.to_i 
+puts ''
+puts '5 is my favorite number!'.to_i 
+puts 'Who asked you about 5 or wahtever?' .to_i 
+puts 'your momma did'.to_f 
+puts ''
+puts'stringy'.to_s
+puts 3.to_i 
+puts 'Hello there, and what\'s your name!'
+name = gets.chomp
+puts 'your name is' + '? whata lovely name!'
+puts 'Pleased to meet you, ' + name + '.:)' 
+
+puts 'hello there what is yo name' 
+name1 = gets.chomp 
+puts 'how lovely' + name + 'what is your middle name'
+name2 = gets.chomp
+puts name1 + name2 + 'what is your last name'
+name3 = gets.chomp 
+puts 'well what a great name' + name1 + name2 + name3 + 'see you'
+
+puts 'what is your favorite number' 
+number = gets 
+puts ' your favorite number should be bigger' + number.to_s
+
